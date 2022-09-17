913dd3a3ec856701797d5e2a30ca127fea4eff9b
jagadeesh allagi <allagijagadeesh.xworkz@gmail.com>
2022-09-17 14:39:28 +0530

    checked the git file

---
+++ Charex.java
@@ -0,0 +1,19 @@
+class Charex{
+	public static void main(String[] number){
+		
+		char c1;
+		char c2;
+		char c3;
+		char c4;
+		
+		
+		c1=28;
+		c2=34;
+		c3=21;
+		c4='i';
+		System.out.println("c1 ,c2 ,c3,c4");
+		System.out.println(c1 +c2 +c3);
+		System.out.println( c1*c3+c2);
+		System.out.println(c2>c1);//true
+	}
+}
