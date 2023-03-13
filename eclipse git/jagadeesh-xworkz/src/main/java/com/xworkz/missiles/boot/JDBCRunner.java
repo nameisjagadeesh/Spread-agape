e71911f7e4b6aeb3e238351eabc9f62c7dc58f95
jagadeesh allagi <allagijagadeesh.xworkz@gmail.com>
2023-03-13 10:05:37 +0530

    insert,update and delete and findByAll via JDBC

--- eclipse git/jagadeesh-xworkz/src/main/java/com/xworkz/missiles/boot/JDBCRunner.java
+++ eclipse git/jagadeesh-xworkz/src/main/java/com/xworkz/missiles/boot/JDBCRunner.java
@@ -14,13 +14,24 @@ public class JDBCRunner {
 			String insert="insert into missile_table values(0,'trishul','DRDO','India',25652,'Military')";
 			String insert2="insert into missile_table values(0,'AGTC','HAL','India',85623,'Military')";
 			String insert3="insert into missile_table values(0,'NISAR','ISRO','India',984632,'communication')";
-
+			String update="update missile_table set country ='Bharat' where id=1 ";
+			String update1="update missile_table set name ='thishuul' where id=3 ";
 			
+            String delete="delete from missile_table where id='5'";
+            String delete1="delete from missile_table where id='6'";
+            
+            		//DELETE FROM  movies WHERE movie_name='lagn';
+            		
+            		
 			Statement statement=connection.createStatement();
 			//0 is given as Id for each insert to do autoIncreamentt
 			int rows=statement.executeUpdate(insert);
 			int rows2=statement.executeUpdate(insert2);
 			int rows3=statement.executeUpdate(insert3);
+			int rows4=statement.executeUpdate(update);
+			int rows5=statement.executeUpdate(update1);
+			int rows6=statement.executeUpdate(delete);
+			int rows7=statement.executeUpdate(delete1);
 			System.out.println("rows inserted "+rows);
 			System.out.println("rows inserted "+rows2);
 			connection.close();
